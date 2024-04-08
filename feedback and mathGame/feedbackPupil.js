0c57725f825139d720e9f0aba3991b46c11a7f6e
shirab13 <132498132+shirab13@users.noreply.github.com>
2024-04-08 22:42:58 +0300

    Delete feedback and mathGame directory
    השתנה

--- feedback and mathGame/feedbackPupil.js
+++
@@ -1,8 +0,0 @@
-function submitFeedback(feeling) {
-    console.log("שם המשתמש:", feeling);
-    // כאן תוכל לשלוח את המשוב לשרת או לשמור אותו במקום מסוים
-    alert("תודה על המשוב - מחכים לראות אותך שוב");
-    window.location.href = "menuS.html"
-}
-
-// עבודה עם שרת הכנסה 
--- feedback and mathGame/feedbackTeacher.js
+++
@@ -1,8 +0,0 @@
-function submitFeedback(feeling) {
-    console.log("שם המשתמש:", feeling);
-    // כאן תוכל לשלוח את המשוב לשרת או לשמור אותו במקום מסוים
-    alert("תודה על המשוב - מחכים לראות אותך שוב");
-    window.location.href = "menuT.html"
-}
-
-// עבודה עם שרת הכנסה 
